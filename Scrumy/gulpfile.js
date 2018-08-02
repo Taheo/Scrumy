2d1e4480cd26afa5c8a44705b0a03ca8a11a030f
Taheo <segieta.michal@gmail.com>
2018-08-02 18:37:08 +0200

    gulp and sass added

---
+++ Scrumy/gulpfile.js
@@ -0,0 +1,15 @@
+﻿/// <binding BeforeBuild='sass' Clean='sass' />
+var gulp = require("gulp"),
+    fs = require("fs"),
+    less = require("gulp-less"),
+    sass = require("gulp-sass");
+
+// other content removed
+var styleSrc = 'Styles/main.scss',
+    styleDest = 'wwwroot/css';
+
+gulp.task("sass", function () {
+    return gulp.src(styleSrc)
+        .pipe(sass())
+        .pipe(gulp.dest(styleDest));
+});
