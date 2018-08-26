801ed596a61336bb9b85ba29dbaa06d07fdc67d4
Taheo <segieta.michal@gmail.com>
2018-08-26 14:59:16 +0200

    task isDone added

---
+++ Scrumy/Data/Migrations/20180826124134_isDone_added_to_task.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace Scrumy.Data.Migrations
+{
+    public partial class isDone_added_to_task : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "isDone",
+                table: "SprintTasks",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "isDone",
+                table: "SprintTasks");
+        }
+    }
+}
