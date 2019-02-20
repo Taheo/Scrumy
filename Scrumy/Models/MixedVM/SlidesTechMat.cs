3bac3128697db4c964b6e00c8010e327ddf1bd0c
Taheo <segieta.michal@gmail.com>
2019-02-20 19:36:16 +0100

    Color theme finished (without font) and changes in models

--- Scrumy/Models/MixedVM/SlidesTechMat.cs
+++ Scrumy/Models/MixedVM/SlidesTechMat.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace Scrumy.Models.AccountViewModels
+namespace Scrumy.Models.MixedVM
 {
     public class SlidesTechMat
     {
