726611d03ced83ff4dc53ad6e1a904d7bb4d946b
Taheo <segieta.michal@gmail.com>
2019-03-20 22:30:46 +0100

    Sprint Plan started and willBeInNextSprint fixed

---
+++ Scrumy/Controllers/SprintController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Scrumy.Data;
+using Scrumy.Models;
+using Scrumy.Models.SprintVM;
+
+namespace Scrumy.Controllers
+{
+    public class SprintController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public SprintController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Create()
+        {
+            //task list z repo
+            var model = _context.SprintTasks.Where(x => x.willBeInNextSprint == true).ToList();
+            return View(model);
+        }
+
+        [HttpPost]
+        public ActionResult Create(SprintPlanVM model)
+        {
+            return View();
+        }
+    }
+}
---
+++ Scrumy/Models/SprintVM/SprintPlanVM.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Scrumy.Models.SprintVM
+{
+    public class SprintPlanVM
+    {
+        public Sprint SprintToCreate { get; set; }
+        public List<SprintTask> TaskToDiscuss { get; set; }
+    }
+}
