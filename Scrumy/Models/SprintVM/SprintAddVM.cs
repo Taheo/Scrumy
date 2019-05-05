7c26a56e57f38c12d551ca27fd442de448299fce
Taheo <segieta.michal@gmail.com>
2019-05-05 14:16:03 +0200

    discuss fix + simple using clearing

--- Scrumy/Models/SprintVM/SprintAddVM.cs
+++ Scrumy/Models/SprintVM/SprintAddVM.cs
@@ -1,12 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.ComponentModel;
 
 namespace Scrumy.Models.SprintVM
 {
     public class SprintAddVM
     {
+        [DisplayName("Insert Target for Sprint")]
         public string SprintTarget { get; set; }
     }
 }
--- Scrumy/Models/SprintVM/SprintPlanVM.cs
+++ Scrumy/Models/SprintVM/SprintPlanVM.cs
@@ -1,8 +1,4 @@
-﻿using Scrumy.Models.SprintTaskViewModel;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 
 namespace Scrumy.Models.SprintVM
 {
